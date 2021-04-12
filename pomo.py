754056ac145d9a4c6b6e5a7483d2dfd2eb95d21a
KlasshDev <KlasshDev@gmail.com>
2021-04-12 12:00:14 +0000

    Got main function in working status
    Timer defaults to 25M,
    User can overide durration by passing an arg when running script.

--- pomo.py
+++ pomo.py
@@ -4,3 +4,22 @@
 #   A timer to run on the bottom of the terminal
 #------------------------------------------------------------------------------
 
+# import time and other dependancies
+import sys
+import time
+
+def countDown(t):
+    t = t * 60
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+if len(sys.argv) > 1:
+    
+    t = sys.argv[1]
+else:
+    t = 25
+countDown(int(t))
