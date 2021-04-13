d27e45ebb05eb9858a91fadbe37b00c8aece8e2f
KlasshDEV <KlasshDEV@gmail.com>
2021-04-13 08:43:49 -0600

    Added 🍅 emoji, get over it

--- pomo.py
+++ pomo.py
@@ -11,7 +11,7 @@ def pomo(minutes):
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print("Pomo -", timer, end="\r")
+        print("🍅 ", timer, end="\r")
         time.sleep(1)
         t -= 1
 
